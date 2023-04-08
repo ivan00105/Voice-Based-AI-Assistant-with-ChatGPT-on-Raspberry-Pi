d5ff8d1f650758e8dcc405b8e45b41bdd1e08337
nonono <johnwong14793@gmail.com>
2023-04-09 05:40:25 +0800

    updates the config example

--- config_example.py
+++ config_example.py
@@ -1,1 +1,7 @@
-access_key = "YOUR_KEY"
+"""
+    This is an example for config.py
+    Create config.py yourself with the configuration below with your own API keys
+"""
+
+porcupine_access_key = "YOUR_KEY"
+gpt_key = "YOUR_KEY"
