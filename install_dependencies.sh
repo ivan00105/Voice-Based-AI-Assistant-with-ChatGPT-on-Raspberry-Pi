b72a631bd737329698c406bc44b5167907244a0e
nonono <johnwong14793@gmail.com>
2023-04-10 21:12:03 +0800

    update play audio by VLC

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo apt-get install -y python3 python3-venv python-pyaudio python3-pyaudio libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev libatlas-base-dev ffmpeg flac requests websockets
+sudo apt-get install -y python3 python3-venv python-pyaudio python3-pyaudio libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev libatlas-base-dev ffmpeg flac
--- src/text_to_speech.py
+++ src/text_to_speech.py
@@ -1,8 +1,8 @@
 from gtts import gTTS
-from playsound import playsound
+import os
 
 def text_to_speech(text, language):
     tts = gTTS(text, lang=language)
     filename = "output.wav"
     tts.save(filename)
-    playsound(filename)
+    os.system(f"vlc {filename}")
