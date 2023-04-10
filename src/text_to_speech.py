80f6f9306f89908c2de60445e86ed450be0355a1
nonono <johnwong14793@gmail.com>
2023-04-11 00:49:26 +0800

    minor changes for subprocess

--- src/text_to_speech.py
+++ src/text_to_speech.py
@@ -1,7 +1,8 @@
 from gtts import gTTS
 import os
+import subprocess
 
 def text_to_speech(text, language, filename="output.wav"):
     tts = gTTS(text, lang=language)
     tts.save(filename)
-    os.system(f"vlc --play-and-exit --no-repeat {filename}")
+    subprocess.Popen(["vlc", "--play-and-exit", "--no-repeat", filename])
